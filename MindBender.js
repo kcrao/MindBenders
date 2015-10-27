d5413f78c00bde1d145007d986895b5ba7e9bd0c
kcrao <kcrao@hotmail.com>
2015-10-27 11:51:31 -0500

    Centered Settings UI, added icons for app store, and score card template

---
+++ MindBender.js
@@ -0,0 +1,410 @@
+var React = require('react-native');
+var {
+  AppRegistry,
+  AsyncStorage,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  SliderIOS,
+  NavigatorIOS,
+  TouchableHighlight
+} = React;
+
+
+var Dimensions = require('Dimensions');
+var windowSize = Dimensions.get('window');
+
+
+var gameLength=0;
+var BOX_REF = "BOX";
+var precomputeStyle = require('precomputeStyle');
+var TimerMixin = require('react-timer-mixin');
+//var UIExplorerButton = require('./UIExplorerButton');
+var arrayIndex = 0;
+var displayBlankImage = true;
+
+/* Chunking randomizer variable setup BEGIN */
+var imageMax = 7;
+
+var idxRandomImage = Math.floor(Math.round((imageMax) * Math.random()));
+var currenImageId = "";
+var totalIntervals=0;
+var gameRound= new Array();
+var i=0;
+var matchCounter=0;
+var dbgString = "";
+var nBackAmount=0;
+var STORAGE_KEY = '@AsyncStorageMindBenders:key';
+
+
+var ButtonIcon = require('react-native-vector-icons/Ionicons');
+
+var ScoreIcon = require ('react-native-vector-icons/Ionicons');
+
+
+var Icon = require('react-native-vector-icons/FontAwesome');
+
+//var blankImage = [require ('image!blank'),];
+
+/*
+var ScoreRow = React.createClass ({
+   render: function () {
+      var scoreStyle = {
+          flexDirection: 'row',
+          backgroundColor: '#D8DDE4',
+          borderWidth: 0.0,
+          borderColor: '#96B5D5',
+          alignSelf: 'center',
+          marginTop: 20,
+        //  marginBottom: 80
+      };
+      return (
+        <View style = {[scoreStyle, this.props.style]}>
+        {this.props.children}
+        </View>
+      );
+   }
+
+});
+
+*/
+
+
+var ButtonRow = React.createClass({
+ render: function() {
+   var imageStyle = {
+     flexDirection: 'row',
+     backgroundColor: '#D8DDE4',
+     borderWidth: 0.0,
+     borderColor: '#96B5D5',
+     alignSelf: 'center',
+     marginTop: 20,
+     marginBottom: 80
+   };
+   return (
+     <View style={[imageStyle, this.props.style]}>
+       {this.props.children}
+     </View>
+   );
+ }
+});
+
+/* Chunking randomizer variable setup END */
+
+
+/* Function definition for chunking and random array population BEGIN */
+function populateArray () {
+
+     console.log("_____brain_buster.dbg_____" );
+     var matchCount=0;
+     while (matchCount<20) {
+
+
+         if( i > 0 )
+         {
+             var Chunk = Math.floor(Math.round((3) * Math.random())) ; // %50 chance num 0 or 1
+
+             if( Chunk  <= 1  )
+             {
+             gameRound[i] = gameRound[i-1];
+                 // console.log( "{",Chunk,"}," , gameRound[i] );
+             }
+         else
+         gameRound[i] = Math.floor(Math.round((3) * Math.random()));
+         } else
+         gameRound[i] = Math.floor(Math.round((3) * Math.random()));
+
+         dbgString = "Round["  + i + "] " + gameRound[i];
+
+
+         if (gameRound[i]==gameRound[i-2])
+           {
+            // console.log ("So far you have " +matchCount + "matches");
+            // console.log ("You have a match for the value " + gameRound[i] +'at' +i );
+            dbgString = dbgString + ", M{" +   matchCount + "}";
+            matchCount++;
+           }
+
+     console.log ( dbgString );
+
+     // console.log ("\n");
+
+     i++;
+  }
+     gameLength=gameRound.length;
+     console.log("_____brain_buster.dbg_____" );
+     console.log ("You needed an array sized at " + gameLength);
+}
+/* Function definition for chunking and random array END */
+
+
+
+
+
+/* Load Images from files */
+/*
+var imageArray =[
+ require('image!spades'),
+ require('image!dice'),
+ require('image!home'),
+ require('image!newspaper'),
+ require('image!home2'),
+ require('image!office'),
+ require('image!pencil'),
+];
+*/
+
+var imageArray = [
+  ['car', '#4F8EF7'],
+  ['anchor', '#4F8EF7'],
+  ['coffee', '#4F8EF7'],
+  ['bell', '#4F8EF7'],
+  ['heart', '#4F8EF7'],
+  ['sun-o', '#4F8EF7'],
+  ['rocket', '#4F8EF7'],
+  ['key', '#4F8EF7'],
+];
+
+
+
+var ScoreCard = React.createClass ({
+
+render: function () {
+    var imgScoreSize = 18;
+   return (
+
+
+     <View style ={styles.scorecard}>
+
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+     <ScoreIcon name = "checkmark-circled"  size={imgScoreSize} color="#005C00" />
+     <ScoreIcon name= "close-circled" size={imgScoreSize} color ="#700000" />
+
+    </View>
+
+
+   )
+
+}
+
+
+});
+
+
+ var MindBender = React.createClass({
+   mixins: [TimerMixin],
+
+   getInitialState() {
+    console.log('in the game nbackamount is' +nBackAmount);
+
+    populateArray();
+    return {
+      arrayIndex: 0
+     }
+   },
+
+
+   componentDidMount() {
+     this._loadInitialState().done();
+
+     this.setInterval(() => {
+       arrayIndex = arrayIndex + 1;
+      // var nativeProps = precomputeStyle({transform: [{rotate: rotation.toString() + "deg"}]});
+    //	this.refs[BOX_REF].setNativeProps(nativeProps);
+    if (arrayIndex===14) {
+      arrayIndex=0;
+    }
+    this.setState({arrayIndex: this.state.arrayIndex + 1});
+
+  },500);
+},
+  // ASYNC load data from persistent storage
+  async _loadInitialState() {
+   try {
+     nBackAmount = await AsyncStorage.getItem(STORAGE_KEY);
+     if (nBackAmount !== null){
+       this.setState({selectedValue: nBackAmount});
+       console.log('Recovered selection from disk: for Game' + nBackAmount);
+       nBackAmount=Number(nBackAmount);
+       console.log ('Reformatted nback =' +nBackAmount);
+
+     } else {
+       console.log('Initialized with no selection on disk. for Game');
+       nBackAmount = 2;
+     }
+   } catch (error) {
+     console.log('AsyncStorage error: ' + error.message);
+   }
+ },
+
+  audioMatchPress: function() {
+    console.log ('Audio Match Button Pressed');
+  },
+
+  imageMatchPress: function () {
+    console.log ('Image Match Button Pressed');
+  },
+
+
+   render: function() {
+
+
+//    var movie = imageArray[arrayIndex];
+    displayBlankImage ^=true;
+    //var {nBackAmount, ...other}= this.props;
+
+    if (displayBlankImage==true) {
+      var movie ='registered';
+      var movieColor = '#D8DDE4';
+    }
+    else  {
+    var imageIndex = gameRound[arrayIndex];
+    var movie = imageArray[imageIndex][0];
+    var movieColor = imageArray[imageIndex][1];
+
+    console.log(imageArray);
+    console.log(arrayIndex);
+  }
+     return (
+       <View style={styles.box} >
+      <View>
+
+      <Text style={{alignSelf: 'center'}}> {'\n'+ nBackAmount + ' back' + '- On Turn ' + 'of ' + gameLength + '\n'} </Text>
+
+
+
+      <Icon name={movie} size={150} style={styles.thumbnail} color={movieColor} />
+
+
+      </View>
+      <View style = {{paddingTop: 20}, {alignItems: 'center'}}>
+      <Text style = {{textAlign: 'center'}, {paddingBottom: 10}}>
+      {'Your Score'}
+      </Text>
+      </View>
+
+     <View style = {{paddingTop: 50}, {borderRadius: 4}, {borderWidth: .5}}>
+
+     <ScoreCard/>
+     <ScoreCard/>
+      </View>
+     <View style = {styles.buttonRowParentStyle}>
+     <View style={styles.buttonRowStyle}>
+
+     <ButtonIcon.Button name="mic-a" size={20} paddingRight={150} backgroundColor="gray" onPress={this.audioMatchPress}>
+
+       <Text style={{ fontWeight: 'bold', color: 'white'}}>{'Audio Match'}</Text>
+
+     </ButtonIcon.Button>
+
+     <ButtonIcon.Button name="images" size={20} backgroundColor="gray" onPress={this.imageMatchPress}>
+
+       <Text style={{ fontWeight: 'bold', color: 'white'}}>{'Image Match'}</Text>
+
+     </ButtonIcon.Button>
+     </View>
+     </View>
+     <View style={styles.footer}>
+     </View>
+    </View>
+     );
+   }
+ });
+
+ var styles = StyleSheet.create({
+   box: {
+     marginTop: 1,
+     //width: this.state.width,
+    // height: this.state.height,
+    alignItems: 'stretch',
+    alignSelf: 'stretch',
+     backgroundColor: '#D8DDE4',
+     paddingTop: 64,
+     //padding: 20,
+     flex: 1
+   },
+   thumbnail: {
+     //marginTop: 100,
+     //marginBottom: 25,
+     //width: 160,
+     //height: 160,
+     borderWidth: 1,
+     borderColor: '#7b8994',
+     alignItems: 'stretch',
+     alignSelf: 'center',
+     //justifyContent: 'center',
+     //resizeMode: 'contain',
+     flex: .5,
+   },
+
+   buttonRowParentStyle: {
+         flex: .25,
+         flexDirection: 'row',
+         paddingTop: 2,
+         paddingLeft: 10,
+         paddingRight:10,
+
+
+   },
+
+   buttonRowStyle: {
+
+     //flexDirection: 'row',
+     flex: 1,
+     flexDirection: 'row',
+     //flexWrap: 'nowrap',
+     //backgroundColor: '#D8DDE4',
+     borderRadius: 4,
+     justifyContent: 'space-around',
+   //  borderWidth: 1,
+
+     borderColor: '#96B5D5',
+
+     alignItems: 'center',
+     alignSelf: 'center',
+     //paddingBottom: 5,
+
+   },
+
+
+  scorecard: {
+     flex: .05,
+     flexDirection: 'row',
+     flexWrap: 'nowrap',
+     borderRadius: 4,
+     alignSelf: 'center',
+     alignItems: 'center',
+     //paddingBottom: 2,
+     //backgroundColor: 'white',
+
+
+  },
+
+   footer: {
+     flex: .25,
+     paddingTop: 20,
+     paddingBottom: 80,
+   }
+
+});
+
+
+module.exports = MindBender;
---
+++ index.ios.js
@@ -0,0 +1,483 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+'use strict';
+var nBackAmount =0;
+ var React = require('react-native');
+ var {
+   AppRegistry,
+   AsyncStorage,
+   StyleSheet,
+   Image,
+   Text,
+   View,
+   SliderIOS,
+   NavigatorIOS,
+   TouchableHighlight,
+   SwitchIOS,
+ } = React;
+
+
+ var Dimensions = require('Dimensions');
+ var windowSize = Dimensions.get('window');
+
+ var BOX_REF = "BOX";
+ var precomputeStyle = require('precomputeStyle');
+ var TimerMixin = require('react-timer-mixin');
+ //var UIExplorerButton = require('./UIExplorerButton');
+ var arrayIndex = 0;
+ var displayBlankImage = true;
+
+
+ /* Access to external icon library */
+ var Icon = require('react-native-vector-icons/MaterialIcons');
+
+
+ /* Async Storage */
+
+ var STORAGE_KEY = '@AsyncStorageMindBenders:key';
+
+
+/* Chunking randomizer variable setup BEGIN */
+var imageMax = 7;
+
+var idxRandomImage = Math.floor(Math.round((imageMax) * Math.random()));
+var currenImageId = "";
+var totalIntervals=0;
+var gameRound= new Array();
+var i=0;
+var matchCounter=0;
+var dbgString = "";
+/* Chunking randomizer variable setup END */
+
+
+/* Function definition for chunking and random array population BEGIN */
+
+
+
+ /*var buttonImageArray = [
+   require ('image!nBackSettings'),
+   require ('image!nbackGridSettings'),
+ ];
+
+ var settingsIcon = [
+   require ('image!settings'),
+ ];
+*/
+ var logo= [
+    require ('image!mindBenderLogo'),
+ ]
+
+ var logoIcon = logo[0];
+// var settingsImage = settingsIcon[0];
+
+// var nBackSettingsButton = buttonImageArray[0];
+ //var nGridSettingsButton = buttonImageArray[1];
+
+ //var blankImage = [require ('image!blank'),];
+
+
+/* Pull in MindBender nBACK IMAGE GAME COMPONENT */
+var MindBender = require ('./MindBender');
+
+/* END Pull in MindBender nBACK IMAGE COMPONENT */
+
+
+ var Slider = React.createClass({
+
+   getInitialState() {
+     this._loadInitialState().done();
+
+     return {
+       value: nBackAmount,
+     };
+   },
+
+   componentDidMount () {
+   this._loadInitialState().done();
+ },
+
+
+ async _loadInitialState() {
+  try {
+    nBackAmount = await AsyncStorage.getItem(STORAGE_KEY);
+    if (nBackAmount !== null){
+      this.setState({selectedValue: nBackAmount});
+      console.log('Recovered selection from disk: ' + nBackAmount);
+      nBackAmount = Number(nBackAmount);
+    } else {
+      console.log('Initialized with no selection on disk.');
+      nBackAmount = 2;
+    }
+  } catch (error) {
+    console.log('AsyncStorage error: ' + error.message);
+  }
+},
+
+  async _handleValueChanged(value) {
+     nBackAmount = Math.round(value);
+
+
+     this.setState({nBackAmount: nBackAmount});
+
+     // save changed value in persistent storage
+
+     try {
+       await AsyncStorage.setItem(STORAGE_KEY, nBackAmount.toString());
+      console.log('Saved selection to disk: ' + nBackAmount.toString());
+         } catch (error) {
+           this._appendMessage('AsyncStorage error: ' + error.message);
+         }
+    // end save changed value in persistent storage
+     },
+
+     render() {
+
+
+       console.log('nbackAmount = ' + nBackAmount);
+
+     return (
+
+       <View style = {styles.sliderContainer}>
+
+         <Text>
+           {' Choose how many values to go back: '}
+        </Text>
+         <Text style={{textAlign: 'center'}}>
+           {'\n Match back: '}
+           {nBackAmount}
+           {' back'}
+         </Text>
+         <SliderIOS
+           style={styles.slider}
+          minimumValue={1}
+          maximumValue={10}
+          value = {Number(nBackAmount)}
+           onValueChange={this._handleValueChanged} />
+
+       </View>
+
+     );
+   }
+ });
+
+ var GameButtonAudio = React.createClass({
+
+   getInitialState: function() {
+     return {
+     trueSwitchIsOn: true,
+     falseSwitchIsOn: false,
+       };
+   },
+   render: function() {
+     return (
+      <View style = {styles.textContainer}>
+       <Text>
+
+       {' Game Type: '}
+       </Text>
+
+
+       <View style={styles.buttonContainer}>
+
+       <Text> {'Audio: '} </Text>
+       <SwitchIOS
+         onValueChange={(value) => this.setState({falseSwitchIsOn: value})}
+         onTintColor= 'blue'
+         thumbTintColor = '#c2c2d6'
+         value={this.state.falseSwitchIsOn} />
+        <Text> {'Video: '} </Text>
+       <SwitchIOS
+         onValueChange={(value) => this.setState({trueSwitchIsOn: value})}
+         onTintColor='blue'
+         thumbTintColor = '#c2c2d6'
+         value={this.state.trueSwitchIsOn} />
+
+       </View>
+       </View>
+     );
+   }
+ });
+
+
+ var Settings = React.createClass({
+
+   componentDidMount() {
+   this._loadInitialState().done();
+ },
+
+ async _loadInitialState() {
+  try {
+    var nBackAmount = await AsyncStorage.getItem(STORAGE_KEY);
+    if (nBackAmount !== null){
+      this.setState({selectedValue: nBackAmount});
+      console.log('Recovered selection from disk: ' + nBackAmount);
+    } else {
+      console.log('Initialized with no selection on disk.');
+      nBackAmount = 2;
+    }
+  } catch (error) {
+    console.log('AsyncStorage error: ' + error.message);
+  }
+},
+
+
+   onNBackPress: function () {
+      this.props.navigator.push ({
+        title: 'nBack',
+        component: MindBender,
+      });
+   },
+
+
+   render: function () {
+      return (
+
+            <View style={[styles.pad, {backgroundColor: '#A0ACB8'}]}>
+
+                        <Slider/>
+
+            <View  style={{paddingTop:30}, {borderRadius:4}, {borderWidth: 1}, {flex: .3}}>
+
+            <View style ={styles.buttonRowParentStyle}>
+            <Text style= {styles.textContainer}>
+            {' Select Game: \n'}
+            </Text>
+
+            <View style ={styles.buttonRowStyle}>
+
+
+            <Icon.Button name="panorama" backgroundColor="#3b5998"  onPress={this.onNBackPress}>
+               nBack Image
+            </Icon.Button>
+
+
+            <Icon.Button name="panorama" backgroundColor="#3b5998"  onPress={this.onNGridPress}>
+               nBack Grid
+            </Icon.Button>
+
+            </View>
+            </View>
+
+            </View>
+
+            <View style={{backgroundColor: '#D8DDE4'}, {paddingTop:20}, {flex:.3}, {borderRadius: 4}, {borderWidth: 1}}>
+
+                        <GameButtonAudio/>
+            </View>
+            <View style={{flex: .2}}>
+            </View>
+
+            </View>
+
+          );
+        }
+
+ });
+
+ var Welcome = React.createClass({
+   onPress: function () {
+      this.props.navigator.push ({
+        title: 'nBack',
+        component: MindBender,
+      });
+   },
+
+
+   render: function () {
+     return (
+       <View style={[styles.pad, {backgroundColor: '#A0ACB8'}]}>
+
+
+      <TouchableHighlight onPress={this.onPress}>
+      <Image source ={logoIcon}
+             style = {styles.logo}>
+
+        </Image>
+      </TouchableHighlight>
+
+       </View>
+
+     )
+   }
+ });
+
+
+var MindBenderNav = React.createClass({
+  onRightButtonPress: function () {
+    this.refs.nav.push({
+      title: 'Settings',
+      component: Settings
+    })
+  },
+
+  render: function () {
+     return (
+       <NavigatorIOS ref="nav" style={styles.container}
+          initialRoute={{
+            component: Welcome,
+            title: 'Welcome',
+            rightButtonTitle: 'Settings',
+            onRightButtonPress: this.onRightButtonPress,
+            translucent: true,
+          }}
+         titleStyle= {styles.navTitle}
+          />
+     );
+
+  }
+});
+
+ var styles = StyleSheet.create({
+
+  slider: {
+  //paddingTop: 20,
+  //paddingBottom: 10,
+  //backgroundColor: '#D8DDE4',
+  //height: 10,
+  //margin: 10,
+  //flex: .5,
+ },
+
+ sliderContainer: {
+  flex: .2,
+  flexDirection: 'column',
+  borderRadius: 4,
+  borderWidth: 1,
+
+ },
+
+
+container: {
+    flex: 1,
+
+  },
+
+ pad: {
+          padding: 20,
+          paddingTop: 64,
+          //marginTop: 20,
+        //  width: windowSize.width,
+        //  height: windowSize.height,
+          flex: 1
+      },
+
+button: {
+
+         width: 150,
+         height: 50,
+         alignSelf: 'center',
+         resizeMode: 'contain',
+
+      },
+
+icon: {
+                alignSelf: 'center',
+                resizeMode: 'contain',
+                fontSize: 20,
+                fontWeight: 'bold'
+
+         },
+
+logo: {
+               //paddingTop: 15,
+               //paddingRight: 10,
+                 width: windowSize.width,
+                 height: windowSize.height,
+              justifyContent: 'center',
+              alignItems: 'stretch',
+               alignSelf: 'center',
+               resizeMode: 'stretch',
+               //borderWidth: 2,
+               borderColor: '#7b8994',
+               flex: 1
+      },
+
+screenTitle: {
+              flexDirection: 'row',
+              paddingBottom: 20,
+        },
+
+  buttonRowParentStyle: {
+        flex: 1,
+        //flexDirection: 'row',
+        borderRadius: 4,
+        borderWidth: 1,
+
+
+  },
+  buttonRowStyle: {
+
+    //flexDirection: 'row',
+    //flex: .2,
+    //flexDirection: 'row',
+  //  flexWrap: 'nowrap',
+    //backgroundColor: '#D8DDE4',
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+
+    borderRadius: 4,
+    justifyContent: 'space-around',
+  //  borderWidth: 1,
+
+    borderColor: '#96B5D5',
+
+    alignItems: 'center',
+    alignSelf: 'auto',
+  //  alignSelf: 'center',
+    paddingBottom: 100,
+  },
+
+  /* Game Buttons */
+
+  navTitle: {
+    fontFamily: 'Karnivore',
+
+
+  },
+
+  textContainer: {
+    paddingTop: 2,
+    flex: 1,
+    //allowFontScaling: false,
+    //backgroundColor: '#D8DDE4',
+
+    //borderRadius: 4,
+    //justifyContent: 'center',
+
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    borderRadius: 4,
+    paddingTop: 10,
+    //paddingBottom: 20,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    alignSelf: 'center',
+    //backgroundColor: '#D8DDE4',
+  },
+  buttonInit: {
+    fontSize: 12,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF'
+  },
+  buttontouchable: {
+    borderRadius: 100
+  },
+  buttonGameType: {
+    backgroundColor: '#FF0000',
+    borderRadius: 30,
+    height: 60,
+    width: 60,
+    justifyContent: 'center'
+  },
+
+ });
+
+
+AppRegistry.registerComponent('MindBenders', () => MindBenderNav);
+module.exports = MindBenderNav;
